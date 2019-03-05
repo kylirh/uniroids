4171ea4a8290bbc627996607f1a9d942c6b5a4ce
Kylir Horton <kylirh@gmail.com>
2019-03-05 12:20:45 -0700

    Add keyboard controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+
+    private void FixedUpdate()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidBody.velocity = movement * speed;
+
+        rigidBody.position = new Vector3
+        (
+            Mathf.Clamp(rigidBody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigidBody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidBody.velocity.x * -tilt);
+    }
+}
