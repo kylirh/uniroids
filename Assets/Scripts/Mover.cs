dc0eeb0ae642fa5c70299119c405760c89af2e34
Kylir Horton <kylirh@gmail.com>
2019-03-07 20:50:02 -0700

    Add laser bolt

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = transform.forward * speed;
+    }
+}
