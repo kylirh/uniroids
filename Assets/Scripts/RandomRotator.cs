07b5fe4af10b377a662de76c72db18488874d4a0
Kylir Horton <kylirh@gmail.com>
2019-03-26 06:36:02 -0600

    Add astroids and destroy on contact with stuff

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+
+    void Start()
+    {
+        var body = GetComponent<Rigidbody>();
+        body.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
