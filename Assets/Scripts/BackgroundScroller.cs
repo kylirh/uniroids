9b0e39ba2d866c08d92718a5910e78a77683e1c5
Kylir Horton <kylirh@gmail.com>
2019-04-11 07:28:00 -0600

    Add starfield and scrolling parallax effect

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSize;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+        transform.position = startPosition + Vector3.forward * newPosition;
+    }
+}
